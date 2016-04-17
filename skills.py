7dc8ab75dab5bf5f6b5985e8bb36380cddcbe088
Ashley Hsiao <ahsiao@Ashleys-MacBook-Pro.local>
2016-04-17 13:04:46 -0700

    Completed Part 2 and 3 on Classes and Methods

--- skills.py
+++ skills.py
@@ -1,5 +1,6 @@
 """Skills Assessment - Object Orientation"""
 
+
 """Part 1: Discussion
 1. The three main design advantages that object orientation can provide are:
     Abstraction - Hides details that we do not need to know regarding methods
@@ -18,3 +19,56 @@
 
 """
 
+
+"""Part 2: Classes and Init Methods + Part 3: Methods"""
+
+
+class Student(object):
+    """Student"""
+
+    def __init__(self, first_name, last_name, address):
+        """Initialize student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+
+class Question(object):
+    """Question"""
+
+    def __init__(self, question, correct_answer):
+        """Initialize question"""
+
+        self.question = question
+        self.correct_answer = correct_answer
+
+    def ask_and_evaluate(self):
+        user_answer = raw_input(self.question + " > ")
+        # print user_answer == self.correct_answer
+        return user_answer == self.correct_answer
+
+
+class Exam(Question):
+    """Exam"""
+    questions = []
+
+    def __init__(self, name):
+        """Initialize exam"""
+        self.name = name
+
+    def add_question(self, question, correct_answer):
+        super(Exam, self).__init__(question, correct_answer)
+        self.questions.append(question)
+
+    def administer(self):
+        score = 0
+
+        for self.question in self.questions:
+            if self.ask_and_evaluate() is True:
+                score += 1
+
+        return score
+
+
+"""Part 4: Create an actual exam!"""
